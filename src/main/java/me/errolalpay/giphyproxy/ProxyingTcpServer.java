82f38020848a315736b915369c0abc81e1f3337e
Errol Alpay <ealpay@gmail.com>
2020-06-16 16:56:49 -0700

    Reverting ByteBuffer.flip

--- src/main/java/me/errolalpay/giphyproxy/ProxyingTcpServer.java
+++ src/main/java/me/errolalpay/giphyproxy/ProxyingTcpServer.java
@@ -182,7 +182,7 @@ public class ProxyingTcpServer extends Thread {
 									throw new EOFException( "Closed connection" );
 								}
 
-								( (Buffer) m_readWriteBuffer ).flip();
+								m_readWriteBuffer.flip();
 								int bytesWritten = 0;
 								while ( m_readWriteBuffer.hasRemaining() ) {
 									bytesWritten += companionChannel.write( m_readWriteBuffer );
