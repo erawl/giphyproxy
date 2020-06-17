ab04b246c1a3ee004ca872032dc4c500a7477267
Errol Alpay <ealpay@gmail.com>
2020-06-16 21:30:20 -0700

    Renaming ProxyingTcpServer to PeerToPeerServer

--- src/main/java/me/errolalpay/giphyproxy/App.java
+++ src/main/java/me/errolalpay/giphyproxy/App.java
@@ -6,16 +6,16 @@ public class App {
 
 		// TODO: consume the command line options for different port numbers, logging etc.
 
-		// instantiate the proxy server
+		// instantiate the peer to peer server
 		// allow incoming requests on 8443 to be proxied to api.giphy.com:443 (https)
-		ProxyingTcpServer proxyServer = new ProxyingTcpServer( 8443, "api.giphy.com", 443 );
+		PeerToPeerServer peerToPeerServer = new ExternalServicePeerToPeerServer( 8443, "api.giphy.com", 443 );
 
 		// start the server
-		proxyServer.start();
+		peerToPeerServer.start();
 
 		// wait for the server to exit
-		synchronized ( proxyServer ) {
-			proxyServer.wait();
+		synchronized ( peerToPeerServer ) {
+			peerToPeerServer.wait();
 		}
 
 		// done
